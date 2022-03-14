96eebd7e43824841875391bce4f5ca2c83517743
Muskan <muskanchhatrasal@gmail.com>
2022-03-14 07:16:31 +0530

    Add - React router

--- wow-shop/src/index.js
+++ wow-shop/src/index.js
@@ -1,15 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+// import App from "./App";
 import { makeServer } from "./server";
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import Home from '../src/Components/Home/home';
+import Products from '../src/Components/Products/products'
+
 
 // Call make Server
 makeServer();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+  <BrowserRouter>
+   <Routes>
+     <Route path="/" element={<Home />} />
+     <Route path="products" element={<Products />} />
+   </Routes>
+  </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
